e6b1615c6a60928fb329fb57c088dcc3288f7e92
sachin <sachinvk867@gmail.com>
2026-04-25 10:18:23 +0530

    e

--- client/src/pages/user/ViewDoc.jsx
+++ client/src/pages/user/ViewDoc.jsx
@@ -173,7 +173,7 @@ const ViewDoc = () => {
             className={`${theme === "dark" ? "bg-gray-800 border-gray-600" : "bg-gray-100 border-gray-300"} border py-0.5 px-1 rounded-sm`}
           >
             <div
-              className={`border ${theme === "dark" ? "border-gray-600" : "border-gray-300"} flex items-center py-0.5 px-1 rounded-sm`}
+              className={`border ${theme === "dark" ? "border-gray-600" : "border-gray-300"} flex items-center py-0.5 px-0.5 rounded-sm`}
             >
               <MdViewSidebar size={16} color={iconColor} />
             </div>
