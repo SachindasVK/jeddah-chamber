80e79ed5f72bacc92694969da6cae50922dea876
sachin <sachinvk867@gmail.com>
2026-04-25 11:22:22 +0530

    down bar added

--- client/src/pages/user/ViewDoc.jsx
+++ client/src/pages/user/ViewDoc.jsx
@@ -162,29 +162,20 @@ const ViewDoc = () => {
         className={`w-full sticky top-0 z-10 flex flex-col gap-2 p-3 shadow-md border ${theme === "dark" ? "bg-[#1e293b] border-gray-700" : "bg-white border-gray-100"}`}
       >
         <div className="flex items-center w-full gap-1.5">
-         <div
-  className={`border py-0.5 px-0.5 rounded-sm `}
->
-  <div
-    className={`border ${
-      theme === "dark" ? "border-gray-600" : "border-gray-300"
-    } flex items-center py-0.5 px-0.5 rounded-sm
-    ${
-    showSidebar
-      ? "bg-blue-800 border-blue-700"
-      : theme === "dark"
-      ? "bg-gray-800 border-gray-600"
-      : "bg-gray-100 border-gray-300"
-  }`}
-  >
-    <MdViewSidebar
-      size={15}
-      color={showSidebar ? "#ffffff" : iconColor}
-      onClick={() => setShowSidebar((prev) => !prev)}
-      className="cursor-pointer"
-    />
-  </div>
-</div>
+          <div
+            className={`${theme === "dark" ? "bg-gray-800 border-gray-600" : "bg-gray-100 border-gray-300"} border py-0.5 px-0.5 rounded-sm`}
+          >
+            <div
+              className={`border ${theme === "dark" ? "border-gray-600" : "border-gray-300"} flex items-center py-0.5 px-0.5 rounded-sm`}
+            >
+              <MdViewSidebar
+                size={15}
+                color={iconColor}
+                onClick={() => setShowSidebar((prev) => !prev)}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
 
           <div
             className={`flex items-center gap-0.5 ${theme === "dark" ? "bg-gray-800 border-gray-600" : "bg-gray-100 border-gray-300"} border p-0.5 rounded-sm`}
