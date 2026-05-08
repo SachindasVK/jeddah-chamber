0c7644ad8e0334af14149eb7c4847a6317ae566b
sachin <sachinvk867@gmail.com>
2026-05-08 21:05:44 +0530

    added

--- server/src/controllers/documentController.js
+++ server/src/controllers/documentController.js
@@ -26,7 +26,7 @@ export const createDocumentAndQR = async (req, res) => {
     const qrUrl = `${process.env.CLIENT_URL}/view/${uniqueId}`;
 
     const qrOptions = {
-      errorCorrectionLevel: "M",
+      errorCorrectionLevel: "Q",
       version:13,
       margin: 3,
       width: 300,
