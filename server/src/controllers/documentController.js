2260c4f50bdd33883403fad08fad2ebfc1a4fcad
sachin <sachinvk867@gmail.com>
2026-05-09 17:17:13 +0530

    cange

--- server/src/controllers/documentController.js
+++ server/src/controllers/documentController.js
@@ -27,6 +27,7 @@ export const createDocumentAndQR = async (req, res) => {
 
     const qrOptions = {
       errorCorrectionLevel: "M",
+      version:15,
       margin: 1,
       width: 800,
     };
