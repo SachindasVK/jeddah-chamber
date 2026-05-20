c440df646d22e204e42844a1f582782afa82426c
sachin <sachinvk867@gmail.com>
2026-05-20 12:41:32 +0530

    pagination

--- server/src/controllers/documentController.js
+++ server/src/controllers/documentController.js
@@ -141,7 +141,7 @@ export const pdfDetails = async (req, res) => {
 export const pdfList = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 5;
+    const limit = parseInt(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     const totalDocs = await Document.countDocuments();
